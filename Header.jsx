d3bb680eb958556f4257e56a0538cc1fa60864e6
KM. PURNIMA VARSHNEY <85434471+purnima0350@users.noreply.github.com>
2021-06-06 14:59:45 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>ShapeAI Bootcamp For React with JS</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React</h1>
+      <p>
+        This was an amazing 7 Days bootcamp conducted by Shaurya Sinha from
+        ShapeAI. I enjoyed it a lot and looking forward to join some more
+        interesting bootcamps.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
